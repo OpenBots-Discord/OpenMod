db88c199acfa452a24394740d4f9e8b4288fa20e
arslee <me@arslee.tk>
2020-08-14 20:40:49 +0900

    remove token :>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """ Configuration file """
 
-TOKEN = 'NzM4Mjc5ODg4Njc0MzU3Mjk4.XyJmug.2nvRwoaNGRYvRlxgNzk7UH3kI6U' # Your bot token
-PREFIX = '--' # Bot prefix
-LANGUAGE = 'ru_RU' # Bot language. Avialable: ru_RU, en_EN
+TOKEN = ''  # Your bot token
+PREFIX = '--'  # Bot prefix
+LANGUAGE = 'ru_RU'  # Bot language. Avialable: ru_RU, en_EN
