1e6bd7d1359b8f4b88697a2370f8f807d5f5b671
Arsenii Liunsha <me@arslee.tk>
2020-08-24 15:11:21 +0900

    removed token... again

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """ Configuration file """
 
-TOKEN = 'NzM4Mjc5ODg4Njc0MzU3Mjk4.XyJmug.ruwN3oG2MX3WT9FQ0GC_MAXtDus'  # Your bot token
+TOKEN = ''  # Your bot token
 DEFAULT_PREFIX = 'm.'  # Bot prefix
 LANGUAGE = 'ru_RU'  # Bot language. Avialable: ru_RU, en_EN
