8a6e4bc267bacc806993ec2e7448e5a59cc33a23
Arsenii Liunsha <me@arslee.dev>
2021-08-28 22:20:41 +0900

    это baza бро

---
+++ lib/shared/instances.dart
@@ -0,0 +1,8 @@
+import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_interactions/interactions.dart';
+
+/// Bot instance
+late Nyxx client;
+
+/// Interactions instance
+late Interactions interactions;
